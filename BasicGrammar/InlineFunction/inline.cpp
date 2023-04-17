0fb8adac5076731b7f59b591f6f818c4a5412cb7
DESKTOP-HHITDO3\Changun <changun516@gmail.com>
2023-04-18 00:00:18 +0900

    feat : inline function study

---
+++ BasicGrammar/InlineFunction/inline.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#define SQU(X) ((X)*(X))
+inline double square(double x) {
+	return x * x;
+}
+
+int main() {
+	double a = 0, b = 0;
+	double c = 4.0;
+	a = square(3.0); // Inline function body runs directly, not function calls
+	b = square(3.0 + 2.0);
+	std::cout << " a = " << a << " b = " << b << std::endl;
+	std::cout << " c = " << c;
+	std::cout <<" cÀÇ Á¦°ö = "<<square(c++)<<std::endl; 
+	std::cout << " c = " << c << std::endl;
+	{
+		std::cout << " c1 : " << c << std::endl;
+		int x = a, y = b;
+		c = x - y;
+		std::cout << c << std::endl;
+	}
+
+	{
+		std::cout << " c2 : " << c << std::endl;
+		// x = c; impossibility
+		int x = c;
+		std::cout << x << std::endl;
+	}
+
+	std::cout << square(3 + 2) << std::endl;
+
+
+
+	return 0;
+}
