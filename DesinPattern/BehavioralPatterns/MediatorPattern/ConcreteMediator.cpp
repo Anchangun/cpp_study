cf5de2b6bb287969e4010400c4a7af7361eb0117
DESKTOP-HHITDO3\Changun <changun516@gmail.com>
2023-05-03 02:32:02 +0900

    refactor : shared_ptr 사용 예제 변경

--- DesinPattern/BehavioralPatterns/MediatorPattern/ConcreteMediator.cpp
+++ DesinPattern/BehavioralPatterns/MediatorPattern/ConcreteMediator.cpp
@@ -1,14 +1,14 @@
 #include "ConcreteMediator.hpp"
 
-void ConcreteMediator::set_user_wavem(UserWavem* wavem) {
+void ConcreteMediator::set_user_wavem(std::shared_ptr<UserWavem> wavem) {
 	m_wavem = wavem;
 }
 
-void ConcreteMediator::set_user_lilac(UserLilac* lilac) {
+void ConcreteMediator::set_user_lilac(std::shared_ptr<UserLilac> lilac) {
 	m_lilac = lilac;
 }
 
-void ConcreteMediator::send(const std::string& message, IColleague* sender){
+void ConcreteMediator::send(const std::string& message, std::shared_ptr<IColleague> sender){
 	if (sender == m_wavem) {
 		m_lilac->recv(message);
 	}
